40ce286fd9c221f630980675952089cda607c530
Kay0n <kayon5555@gmail.com>
2024-08-08 00:16:44 +0930

    sync

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from Dot2Controller import Dot2Controller, ExecutorType, ExecutorGroup
 
 dot2 = Dot2Controller()
 platformM = PMPController()
-queue = asyncio.Queue()
+dot2Queue = asyncio.Queue()
+
 
 dot2.set_executor_groups([
     ExecutorGroup(1, 8, ExecutorType.FADER),
@@ -15,22 +16,28 @@ dot2.set_executor_groups([
 
 
 def dot2_fader_changed(executor_number: int, is_active: bool, normalized_value: float):
-    print(f"Fader {executor_number}, {is_active}, {normalized_value}")
-    # mapped_num = 8 - executor_number
-    # print(f"SETTING PLATFORM({mapped_num}, {normalized_value:.3f})")
-    # platformM.set_fader(mapped_num, normalized_value)
-    # platformM.set_button(mapped_num + 8, is_active)
+    mapped_num = 8 - executor_number
+    print(f"SETTING PLATFORM FADER({mapped_num}, {normalized_value:.3f})")
+    platformM.set_fader(mapped_num, normalized_value)
+    platformM.set_button(mapped_num + 8, is_active)
         
 def dot2_button_changed(executor_number: int, is_active: bool):
-    print(f"Button {executor_number}, {is_active}")
-    # platformM.set_fader(mapped_num, normalized_value)
-    # platformM.set_button(mapped_num + 8, is_active)
+    mapped_num = 8 - executor_number
+    print(f"SETTING PLATFORM BUTTON({mapped_num}, {is_active})")
+    platformM.set_button(mapped_num + 8, is_active) # FIXME button mapping
 
-def pmp_platform_changed(fader_number: int, normalized_value: float):
+def pmp_fader_changed(fader_number: int, normalized_value: float):
     mapped_num = 8 - fader_number
-   
-    queue.put_nowait([mapped_num, normalized_value])
+    print(f"SETTING DOT2 FADER({mapped_num}, {normalized_value:.3f})")
+    dot2Queue.put_nowait([ExecutorType.FADER, mapped_num, normalized_value])
     platformM.set_fader(fader_number, normalized_value)
+
+
+def pmp_button_changed(button_number: int, is_pressed: bool, button_state: bool): # FIXME all
+    mapped_num = 8 - button_number
+    print(f"SETTING DOT2 BUTTON({mapped_num}, {is_active})")
+    dot2Queue.put_nowait([ExecutorType.BUTTON, mapped_num, is_active])
+    platformM.set_button(button_number, is_active)
 
 
 
@@ -38,7 +45,7 @@ async def main():
 
     dot2.add_fader_event_listener(dot2_fader_changed)
     dot2.add_button_event_listener(dot2_button_changed)
-    platformM.add_event_listener(PMPEvent.FADER, pmp_platform_changed)
+    platformM.add_event_listener(PMPEvent.FADER, pmp_button_changed)
 
     platformM.connect()
     await dot2.connect("127.0.0.1", "test", 0.08)
@@ -47,8 +54,8 @@ async def main():
         while True:
             await asyncio.sleep(0.1)
             
-            while not queue.empty():
-                mapped_num, normalized_value = await queue.get()
+            while not dot2Queue.empty():
+                mapped_num, normalized_value = await dot2Queue.get()
                 print(f"SETTING DOT2({mapped_num}, {normalized_value:.3f})")
                 await dot2.set_fader(mapped_num, normalized_value)
     finally:
