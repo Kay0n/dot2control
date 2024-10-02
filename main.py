21c6b9fb9f3dbc3380559d85e59169f1cc02dbb7
Kay0n <kayon5555@gmail.com>
2024-10-03 00:27:48 +0930

    feat: full function

--- main.py
+++ main.py
@@ -1,124 +1,135 @@
 import asyncio
-import aiohttp
-import sys
 from pmpcontroller import PMPController, PMPEvent
 from Dot2Controller import Dot2Controller, ExecutorType, ExecutorGroup
 
-dot2 = Dot2Controller()
-platform_m = PMPController()
-dot2_fader_queue = asyncio.Queue()
-
-dot2.set_executor_groups([
-    ExecutorGroup(1, 8, ExecutorType.FADER),
-    # ExecutorGroup(101, 8, ExecutorType.BUTTON),
-    # ExecutorGroup(201, 8, ExecutorType.BUTTON)
-])
 
 
+class Dot2PMPSync:
+    def __init__(self):
+        self.dot2 = Dot2Controller()
+        self.platform_m = PMPController()
+        self.dot2_fader_queue = asyncio.Queue()
+        self.dot2_button_queue = asyncio.Queue()
 
-def dot2_fader_changed(executor_number: int, is_active: bool, normalized_value: float):
-    if not platform_m.is_connected(): return
-    mapped_num = 8 - executor_number
-    print(f"SETTING PLATFORM FADER({mapped_num}, {normalized_value:.3f})")
-    platform_m.set_fader(mapped_num, normalized_value)
-    platform_m.set_button(mapped_num + 8, is_active)
-        
-# def dot2_button_changed(executor_number: int, is_active: bool):
-#     mapped_num = 8 - executor_number
-#     print(f"SETTING PLATFORM BUTTON({mapped_num}, {is_active})")
-#     platformM.set_button(mapped_num + 8, is_active) # FIXME button mapping
+        self.dot2.set_executor_groups([
+            ExecutorGroup(1, 8, ExecutorType.FADER),
+            ExecutorGroup(101, 8, ExecutorType.BUTTON),
+            ExecutorGroup(201, 8, ExecutorType.BUTTON)
+        ])
 
-
-
-def pmp_fader_changed(fader_number: int, normalized_value: float):
-    if not dot2.is_connected(): return
-    mapped_num = 8 - fader_number
-    if(mapped_num < 1):
-        return
-    print(f"QUEUEING DOT2 FADER({mapped_num}, {normalized_value:.3f})")
-    dot2_fader_queue.put_nowait([ExecutorType.FADER, mapped_num, normalized_value])
-
-    # platformM.set_fader(fader_number, normalized_value)
-
-# def pmp_button_changed(button_number: int, is_pressed: bool, button_state: bool): # FIXME all
-#     mapped_num = 8 - button_number
-#     print(f"SETTING DOT2 BUTTON({mapped_num}, {is_active})")
-#     dot2Queue.put_nowait([ExecutorType.BUTTON, mapped_num, is_active])
-#     platformM.set_button(button_number, is_active)
+        self.dot2.add_fader_event_listener(self.dot2_fader_changed)
+        self.dot2.add_button_event_listener(self.dot2_button_changed)
+        self.platform_m.add_event_listener(PMPEvent.FADER, self.pmp_fader_changed)
+        self.platform_m.add_event_listener(PMPEvent.BUTTON, self.pmp_button_changed)
 
 
-
-def connect_to_pmp() -> bool :
-    try:
-        platform_m.connect()
-    except OSError:
-        return False
-    return True
-
+    def map_dot2_btn_to_pmp(self, button_num):
+        if 101 <= button_num <= 108:
+            return 24 + (108 - button_num)
+        elif 201 <= button_num <= 208:
+            return 16 + (208 - button_num)
+        raise ValueError("Button number must be in the range 101-108 or 201-208")
 
 
-async def connect_to_dot2() -> bool:
-    try:
-        await dot2.connect("127.0.0.1", "password")
-    except OSError as e:
-        return False
-    return True
+    def map_pmp_btn_to_dot2(self, button_num):
+        if 24 <= button_num <= 31:
+            return 108 - (button_num - 24)
+        elif 16 <= button_num <= 23:
+            return 208 - (button_num - 16)
+        raise ValueError("Button number must be in the range 16-31")
 
 
-
-async def update_dot2():
-    while not dot2_fader_queue.empty():
-        executor_type, executor_number, normalized_value = await dot2_fader_queue.get()
-        if executor_type == ExecutorType.FADER:
-            print(f"SETTING DOT2 FADER({executor_number}, {normalized_value:.3f})")
-            await dot2.set_fader(executor_number, normalized_value)
-        elif executor_type == ExecutorType.BUTTON:
-            pass
+    def dot2_fader_changed(self, executor_number: int, is_active: bool, normalized_value: float):
+        if not self.platform_m.is_connected(): return
+        mapped_num = 8 - executor_number
+        self.platform_m.set_fader(mapped_num, normalized_value)
+        self.platform_m.set_button(mapped_num + 8, is_active)  # SOLO button lights green when fader > 0
 
 
-dot2.add_fader_event_listener(dot2_fader_changed)
-platform_m.add_event_listener(PMPEvent.FADER, pmp_fader_changed)
+    def dot2_button_changed(self, executor_number: int, is_active: bool):
+        if not self.platform_m.is_connected(): return
+        try:
+            mapped_num = self.map_dot2_btn_to_pmp(executor_number)
+        except ValueError: return
+        self.platform_m.set_button(mapped_num, is_active)
 
 
+    def pmp_fader_changed(self, fader_number: int, normalized_value: float):
+        if not self.dot2.is_connected(): return
+        mapped_num = 8 - fader_number
+        if mapped_num >= 1:
+            self.dot2_fader_queue.put_nowait([mapped_num, normalized_value])
 
-async def main(): 
-    async def try_connect():
+
+    def pmp_button_changed(self, button_number: int, is_pressed: bool, button_state: bool):
+        if not self.dot2.is_connected() or not is_pressed: return
+        try:
+            mapped_num = self.map_pmp_btn_to_dot2(button_number)
+        except ValueError: return
+        self.dot2_button_queue.put_nowait([mapped_num, not button_state])
+
+
+    async def connect_to_pmp(self):
+        try:
+            self.platform_m.connect()
+            return True
+        except OSError:
+            return False
+
+
+    async def connect_to_dot2(self):
+        try:
+            await self.dot2.connect("127.0.0.1", "password")
+            return True
+        except OSError:
+            return False
+
+
+    async def update_dot2(self):
+        while not self.dot2_fader_queue.empty():
+            executor_number, normalized_value = await self.dot2_fader_queue.get()
+            await self.dot2.set_fader(executor_number, normalized_value)
+        while not self.dot2_button_queue.empty():
+            executor_number, new_state = await self.dot2_button_queue.get()
+            await self.dot2.set_button(executor_number, new_state)
+
+
+    async def try_connect(self):
         while True:
-            await disconnect_all()
-            if not connect_to_pmp():
-                print("Could not connect to pmp, waiting 10s...")
-                await asyncio.sleep(5)
-            elif not await connect_to_dot2():
-                print("Could not connect to dot2, waiting 10s...")
-                await asyncio.sleep(5)
+            if not await self.connect_to_pmp(): name = "pmp"
+            elif not await self.connect_to_dot2(): name = "dot2"
             else:
                 print("Connected, now syncing Dot2 to Platform M+")
-                break
-
-    async def disconnect_all():
-        await dot2.disconnect()
-        if platform_m.is_connected():
-            platform_m.reset()      
-        platform_m.disconnect()
+                self.platform_m.set_button(86,True) # blue indicator light to show sync
+                return
+            print(f"Could not connect to {name}, waiting 10s...")
+            await self.disconnect_all()
+            await asyncio.sleep(10)
 
 
-    try:
-        while True:
-            await try_connect()
-            try:
-                while platform_m.is_connected() and dot2.is_connected():
-                    await update_dot2()
-                    await asyncio.sleep(0.005)
-
-            except ConnectionAbortedError:
-                pass
+    async def disconnect_all(self):
+        await self.dot2.disconnect()
+        if self.platform_m.is_connected():
+            self.platform_m.reset()
+        self.platform_m.disconnect()
 
 
-    finally:
-        await disconnect_all()
-        sys.exit()
+    async def run(self):
+        try:
+            while True:
+                await self.try_connect()
+                try:
+                    while self.platform_m.is_connected() and self.dot2.is_connected():
+                        await self.update_dot2()
+                        await asyncio.sleep(0.005)
+                except ConnectionAbortedError:
+                    pass
+        finally:
+            await self.disconnect_all()
+            exit()
 
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    sync = Dot2PMPSync()
+    asyncio.run(sync.run())
